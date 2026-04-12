8257bb1939c1d2ccbcc533f9f7d2025b026ea5fe
Nate Cheney <natecheney@proton.me>
2026-04-12 16:34:33 +0000

    first commit

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+#########################
+# Python Setup          #
+#########################
+
+echo "Installing Python via apt..."
+sudo apt-get update
+sudo apt-get install -y  python3 \
+    python3-dev \
+    python3-pip \
+    python3-venv
+sudo rm -rf /var/lib/apt/lists/*
+
+if [ ! -d ".venv/" ]; then 
+    echo "Creating Python virtual environment..."
+    python3 -m venv .venv/
+fi
+
+
+echo "Installing packages..."
+.venv/bin/python3 -m pip install --upgrade pip 
+.venv/bin/python3 -m pip install -r .devcontainer/requirements.txt
+
+
+#########################
+# Jupyter Setup         # 
+#########################
+
+echo "Installing Jupyter..."
+.venv/bin/python3 -m pip install --upgrade pip
+.venv/bin/python3 -m pip install jupyter ipykernel
+.venv/bin/python3 -m ipykernel install --user --name=venv_python --display-name="Python 3"
